52b88cece1490ffd59923689609e839b0a7b8ba7
Wojtek <koziolwojciech99@gmail.com>
2022-08-20 11:30:48 +0200

    Plugins clean/repair/update

--- constants/API.js
+++ constants/API.js
@@ -1,14 +1,14 @@
 export const API ={
 
-    getGenerated: "https://niesamowityprojekt.000webhostapp.com/AM_LOGIN/getGenerated.php",
-    getScanned: "https://niesamowityprojekt.000webhostapp.com/AM_LOGIN/getScanned.php",
+    getGenerated: "http://192.168.43.139/AM_LOGIN/getGenerated.php",
+    getScanned: "http://192.168.43.139/AM_LOGIN/getScanned.php",
 
-    addGenerated: "https://niesamowityprojekt.000webhostapp.com/AM_LOGIN/addGenerated.php",
-    addScanned: "https://niesamowityprojekt.000webhostapp.com/AM_LOGIN/addScanned.php",
+    addGenerated: "http://192.168.43.139/AM_LOGIN/addGenerated.php",
+    addScanned: "http://192.168.43.139/AM_LOGIN/addScanned.php",
 
 
-    login: "https://niesamowityprojekt.000webhostapp.com/AM_LOGIN/login.php",
-    signup: "https://niesamowityprojekt.000webhostapp.com/AM_LOGIN/SignUp.php",
+    login: "http://192.168.43.139/AM_LOGIN/login.php",
+    signup: "http://192.168.43.139/AM_LOGIN/SignUp.php",
 }
 
 export default API;
